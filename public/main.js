f93683cb9ad885a0df597238e61845db1e5b71f8
Robert A <bobby@Roberts-MacBook-Pro.local>
2024-02-12 18:05:47 +0000

    Done

---
+++ public/main.js
@@ -0,0 +1,31 @@
+async function init() {
+  let rustApp = null;
+
+  try {
+    rustApp = await import("../pkg");
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+
+  // console.log(rustApp);
+
+  const input = document.getElementById("upload");
+  const fileReader = new FileReader();
+
+  fileReader.onloadend = () => {
+    let base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ""
+    );
+
+    let imageDataUrl = rustApp.grayscale(base64);
+    document.getElementById("new-img").setAttribute("src", imageDataUrl);
+  };
+
+  input.addEventListener("change", () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
+}
+
+init();
